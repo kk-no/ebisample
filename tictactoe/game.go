0e0aa471bfdd7d4541f5df3091b8f462d75dc834
Kohei Keino <kenokohe@gmail.com>
2022-05-10 11:46:43 +0900

    add winner check and internal game board

--- tictactoe/game.go
+++ tictactoe/game.go
@@ -2,6 +2,7 @@ package tictactoe
 
 import (
 	"bytes"
+	"fmt"
 	"image"
 	"image/png"
 	"os"
@@ -10,17 +11,28 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-type Turn int
+type TurnPlayer int
 
 const (
-	Circle Turn = 0
-	Cross  Turn = 1
+	Circle TurnPlayer = 0
+	Cross  TurnPlayer = 1
 )
 
-type Game struct {
-	images *Images
+func (t TurnPlayer) String() string {
+	switch t {
+	case Circle:
+		return "○"
+	case Cross:
+		return "×"
+	default:
+		return ""
+	}
+}
 
-	turn Turn
+type Game struct {
+	images     *Images
+	board      [3][3]string
+	turnPlayer TurnPlayer
 }
 
 type Images struct {
@@ -50,7 +62,7 @@ func New() (*Game, error) {
 			circle: ebiten.NewImageFromImage(circle),
 			cross:  ebiten.NewImageFromImage(cross),
 		},
-		turn: Circle,
+		turnPlayer: Circle,
 	}, nil
 }
 
@@ -64,16 +76,15 @@ func loadImage(name string) (image.Image, error) {
 
 func (g *Game) Update() error {
 	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
-		mx, my := ebiten.CursorPosition()
-		opts := &ebiten.DrawImageOptions{}
-		opts.GeoM.Translate(float64(mx)-67, float64(my)-67)
-		switch g.turn {
-		case Circle:
-			g.images.game.DrawImage(g.images.circle, opts)
-		case Cross:
-			g.images.game.DrawImage(g.images.cross, opts)
+		x, y := ebiten.CursorPosition()
+		g.DrawSymbol(x, y)
+		g.SetBoardSymbol(x, y)
+
+		if winner := g.CheckWinner(); winner != "" {
+			fmt.Printf("%s win!\n", winner)
+			return nil
 		}
-		g.turn ^= 1
+		g.turnPlayer ^= 1 // XOR
 	}
 	return nil
 }
@@ -83,6 +94,43 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.DrawImage(g.images.game, nil)
 }
 
+func (g *Game) DrawSymbol(x, y int) {
+	opts := &ebiten.DrawImageOptions{}
+	opts.GeoM.Translate(float64(x-67), float64(y-67))
+	switch g.turnPlayer {
+	case Circle:
+		g.images.game.DrawImage(g.images.circle, opts)
+	case Cross:
+		g.images.game.DrawImage(g.images.cross, opts)
+	}
+}
+
+func (g *Game) SetBoardSymbol(x, y int) {
+	g.board[x/160][y/160] = g.turnPlayer.String()
+}
+
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return WindowWidth, WindowHight
+}
+
+func (g *Game) CheckWinner() string {
+	for i := range g.board {
+		if IsEqual(g.board[i][0], g.board[i][1], g.board[i][2]) {
+			return g.turnPlayer.String()
+		}
+		if IsEqual(g.board[0][i], g.board[1][i], g.board[2][i]) {
+			return g.turnPlayer.String()
+		}
+	}
+	if IsEqual(g.board[0][0], g.board[1][1], g.board[2][2]) {
+		return g.turnPlayer.String()
+	}
+	if IsEqual(g.board[0][2], g.board[1][1], g.board[2][0]) {
+		return g.turnPlayer.String()
+	}
+	return ""
+}
+
+func IsEqual(s1, s2, s3 string) bool {
+	return s1 != "" && s1 == s2 && s2 == s3
 }
